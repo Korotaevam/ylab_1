32e323f51e76b945a0f656902017f9005362aac2
Korotaevam <kam888@mail.ru>
2023-07-24 16:39:08 +0300

    add timeout 20

--- app.sh
+++ app.sh
@@ -2,7 +2,7 @@
 
 # Ожидать доступности базы данных до 30 секунд
 echo "Проверка доступности базы данных..."
-timeout 30s bash -c 'until echo > /dev/tcp/db/5432; do sleep 1; done'
+timeout 20s bash -c 'until echo > /dev/tcp/db/5432; do sleep 1; done'
 
 # Применить миграции Alembic
 alembic upgrade head
